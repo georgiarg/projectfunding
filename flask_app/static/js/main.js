14f42d6d94a60e4c858dcb23bda4706c4df2bc31
georgiarg <62709554+georgiarg@users.noreply.github.com>
2022-05-30 16:24:26 +0300

    Delete flask_app directory

--- flask_app/static/js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).on("click", ".researcher-row", function() {
-    var path   = window.location.href;
-    id_num = $(this)[0].children[2].innerText 
-    url = path + "/researcherform?researcher_id=" + id_num;
-    window.location = url;
-});
